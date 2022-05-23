ffe8cef1f5d3560abc09383bd3f17578c0813e59
Fabian Martinez <jorgefabianmartinez@gmail.com>
2022-05-23 19:12:49 -0300

    primera entrega

--- js/proyecto.js
+++ js/proyecto.js
@@ -1,47 +1,103 @@
-const ingresoUsuarios = () => {
-    const nombre = prompt("ingrese su nombre");
-    const apellido = prompt("ingrese su apellido");
-    const edad = Number(prompt("ingrese su edad"));
+class Usuario {
+    constructor(id, nombre, apellido, edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
 
-    alert(`Bienvenido ${ nombre + " " + apellido}  a divertirse!! `);
+    }
 
-    return (edad)
+}
+let usuarios = [];
+acciones();
+
+function acciones() {
+    let accion = 0;
+    while (accion !== 2) {
+
+
+        accion = Number(prompt(
+            `Seleccione una accion:
+        1. Agregar Usuario
+        2. Salir`
+        ));
+        switch (accion) {
+            case 1:
+                ingresoUsuarios();
+                break;
+        }
+    }
+}
+
+function ingresoUsuarios() {
+    let id = usuarios.length + 1;
+    let nombre = prompt("ingrese su nombre");
+    let apellido = prompt("ingrese su apellido");
+    let edad = Number(prompt("ingrese su edad"));
+    let usuario = new Usuario(id, nombre, apellido, edad);
+    usuarios.push(usuario);
+
+    alert(`Bienvenido ${nombre + " " + apellido}  a divertirse!! `);
+
+
+    return (edad);
+
+
+
 
 
 }
-ingresoUsuarios()
+let cantidadUsuarios = usuarios.length
+alert(`${cantidadUsuarios} usuarios Jugando`)
+
+const cuestionario = [{
+        pregunta: "¿cual es el color verde?",
+        opciones: ["Color1 ", "  Color2 ", "  Color3 "],
+        respuestaOk: "Color2",
+    },
+    {
+        pregunta: "¿cual es el color rojo?",
+        opciones: ["  Color1 ", "  Color2 ", "  Color3 "],
+        respuestaOk: "Color3",
+    },
+    {
+        pregunta: "¿cual es el color azul?",
+        opciones: ["  Color1 ", "  Color2 ", "  Color3 "],
+        respuestaOk: "Color1",
+    },
+];
+alert(cuestionario[0].pregunta)
+alert(cuestionario[0].opciones)
 
 
-alert(`¿cual es el color verde ?
-
-         opcion1
-         opcion2
-         opcion3`)
-
-
-
-let respuestaCorrecta = false
+respuestaCorrecta = false
+let index = 0;
 while (respuestaCorrecta !== true) {
+    alert(cuestionario[index].pregunta);
+    alert(cuestionario[index].opciones);
+
     let respuestaIngresada = prompt("ingrese su respuesta")
-    let respuesta = opcionCorrecta(respuestaIngresada)
+    let respuesta = opcionCorrecta(respuestaIngresada, index)
 
     if (respuesta) {
         alert("excelente, sigamos con la proxima pregunta")
-        respuestaCorrecta = true;
+        if ((cuestionario.length - 1) == index) {
+            respuestaCorrecta = true;
+        }
+        index++;
 
     } else {
         alert("estuviste cerca, vuelve a intentarlo")
     }
 }
 
-function opcionCorrecta(respuestaIngresada) {
+function opcionCorrecta(respuestaIngresada, index) {
 
 
 
-    if (respuestaIngresada === "opcion3") {
+    if (respuestaIngresada == (cuestionario[index].respuestaOk)) {
         return true;
     } else {
         return false;
     }
 }
-opcionCorrecta()
